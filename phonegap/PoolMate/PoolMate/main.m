534a5c9affebbd620191cbe8663e8976fff17b9d
parkerkrhoyt <khoyt@adobe.com>
2012-01-24 16:45:51 -0700

    Started work on PoolMate.
    PoolMate demonstrates using accelerometer, camera capture and file access to measure a physical distance (pool length).

---
+++ phonegap/PoolMate/PoolMate/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  PoolMate
+//
+//  Created by khoyt on 1/23/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
